b7bd220daf2f2866bf7116d23e142eb2bb4f0bea
ruirui14 <2230103@ecc.ac.jp>
2025-12-12 18:35:34 +0900

    ButtonBaseコンポーネント試作

--- src/component/atoms/ButtonBase.jsx
+++ src/component/atoms/ButtonBase.jsx
@@ -1,4 +1,20 @@
 //ボタンの背景
-function ButtonBase() {
-    return 
+import "../styles/ButtonBase.css";
+import PropTypes from "prop-types";
+
+function ButtonBase({ onClick, id, children }) {
+  return (
+    <button type="button" id={id} onClick={onClick}>
+      {children}
+      <p className="arrow">＞</p>
+    </button>
+  );
 }
+
+ButtonBase.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  id: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default ButtonBase;
