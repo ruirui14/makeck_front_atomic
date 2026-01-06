bb7f935312836aeec8f58ebc7f857ea30feb846c
Haruki <k.haruki.zephyranthes@gmail.com>
2026-01-06 15:26:02 +0900

    CookTimeコンポーネント作成

--- src/component/atoms/CookTime.jsx
+++ src/component/atoms/CookTime.jsx
@@ -1,4 +1,14 @@
 //合計料理時間表示
-function CookTime() {
-  return;
+import PropTypes from "prop-types";
+
+CookTime.propTypes = {
+  time: PropTypes.number,     // 調理時間
+};
+
+function CookTime({ time }) {
+  return(
+    <div id="cookingTime">調理時間目安 : { time? time: "　-　" } 分</div>
+  );
 }
+
+export default CookTime;
