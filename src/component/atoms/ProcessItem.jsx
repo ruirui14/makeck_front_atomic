2af4cd9c8f743a5206cd0e7584ced54ff2af6da6
Haruki <k.haruki.zephyranthes@gmail.com>
2026-01-16 10:27:00 +0900

    ガントチャート内アイテムコンポーネント作成

--- src/component/atoms/ProcessItem.jsx
+++ src/component/atoms/ProcessItem.jsx
@@ -1,4 +1,32 @@
 //ガントチャートのブロック
-function ProcessItem() {
-  return;
+import PropTypes from "prop-types";
+
+ProcessItem.propTypes = {
+  key: PropTypes.string,
+  className: PropTypes.string,
+  taskName: PropTypes.string,
+  time: PropTypes.number,
+  onClick: PropTypes.func,
 }
+
+/**
+ * ガントチャート内要素
+ * @param {string}  key
+ * @param {string}  className  表示区分(手順 手順タイプ | 空き時間)
+ * @param {string}  taskName   手順名
+ * @param {number}  time       持ち時間
+ * @param {func}    onClick    押下時イベント
+ * @returns 手順ボックス または 空き時間の縦線
+ */
+function ProcessItem({ key, className, taskName, time, onClick }) {
+  return(
+    <div
+      key={key}
+      className={`gridItem ${className}`}
+      style={{height: `${time? time * 100 : 2}%`}}
+      onClick={onClick}
+    >{taskName}</div>
+  );
+}
+
+export default ProcessItem;
