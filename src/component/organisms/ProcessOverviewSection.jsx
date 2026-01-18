bcf9aeb02e3712cb7cc07ecbc39dfcf835e377f7
Haruki <k.haruki.zephyranthes@gmail.com>
2026-01-18 14:28:48 +0900

    ProcessOverviewSection(ガントチャート上説明欄)作成

--- src/component/organisms/ProcessOverviewSection.jsx
+++ src/component/organisms/ProcessOverviewSection.jsx
@@ -1,4 +1,19 @@
+import PropTypes from "prop-types";
+import ChartRecipeImages from "../molecules/ChartRecipeImages";
+import ProcessType from "../molecules/ProcessType";
+
 // 調理手順ガントチャート 説明エリア
-function ProcessOverviewSection() {
-    return
+function ProcessOverviewSection({menu}) {
+    return(
+        <>
+            <ProcessType />
+            <ChartRecipeImages menu={menu} />
+        </>
+    )
 }
+
+ProcessOverviewSection.propTypes = {
+    menu: PropTypes.object
+}
+
+export default ProcessOverviewSection;
