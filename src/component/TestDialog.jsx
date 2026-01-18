febeb3f4d4965d0277b59b007b6c8a63942a1d9a
ruirui14 <2230103@ecc.ac.jp>
2026-01-19 03:41:06 +0900

    SelectDialogSectionコンポーネント完成

--- src/component/TestDialog.jsx
+++ src/component/TestDialog.jsx
@@ -1,24 +1,24 @@
-import _React from 'react';
-import { _useNavigate } from "react-router-dom";
-import CustomDialog from "./CustomDialog";
-import PropTypes from "prop-types";
+// import _React from 'react';
+// import { _useNavigate } from "react-router-dom";
+// import CustomDialog from "./CustomDialog";
+// import PropTypes from "prop-types";
 
-export default function TestDialog({ isOpen, onCancel, test_content }) {
-  const content = "テストカスタムダイアログ";
+// export default function TestDialog({ isOpen, onCancel, test_content }) {
+//   const content = "テストカスタムダイアログ";
 
-  return (
-    <CustomDialog
-      isOpen={isOpen}
-      content={content}
-      cancelButtonLabel="キャンセル"
-      onCancel={onCancel}
-      test_content={test_content}
-    />
-  );
-}
+//   return (
+//     <CustomDialog
+//       isOpen={isOpen}
+//       content={content}
+//       cancelButtonLabel="キャンセル"
+//       onCancel={onCancel}
+//       test_content={test_content}
+//     />
+//   );
+// }
 
-  TestDialog.propTypes = {
-    isOpen: PropTypes.bool.isRequired,
-    onCancel: PropTypes.func.isRequired,
-    test_content: PropTypes.array,
-  };
+//   TestDialog.propTypes = {
+//     isOpen: PropTypes.bool.isRequired,
+//     onCancel: PropTypes.func.isRequired,
+//     test_content: PropTypes.array,
+//   };
--- src/pages/TestDialog.jsx
+++ src/pages/TestDialog.jsx
@@ -1,29 +1,28 @@
-import CustomDialog from "./CustomDialog";
-import PropTypes from "prop-types";
+// import CustomDialog from "./CustomDialog";
+// import PropTypes from "prop-types";
 
-export default function TestDialog({ isOpen,test_content, onConfirm, onCancel }) {
-    const content = "～ 選択中レシピ ～";
+// export default function TestDialog({ isOpen,test_content, onConfirm, onCancel }) {
+//     const content = "～ 選択中レシピ ～";
   
-    return (
-      <CustomDialog
-        isOpen={isOpen}
-        content={
-          <div className='dialogContent'>
-          {content}
-          </div>
-        }
-        test_content={test_content}
-        // confirmButtonLabel="OK"
-        cancelButtonLabel="閉じる"
-        onConfirm={onConfirm}
-        onCancel={onCancel}
-      />
-    );
-}
+//     return (
+//       <CustomDialog
+//         isOpen={isOpen}
+//         content={
+//           <div className='dialogContent'>
+//           {content}
+//           </div>
+//         }
+//         test_content={test_content}
+//         cancelButtonLabel="閉じる"
+//         onConfirm={onConfirm}
+//         onCancel={onCancel}
+//       />
+//     );
+// }
 
- TestDialog.propTypes = {
-   isOpen: PropTypes.bool.isRequired,
-   onConfirm: PropTypes.func, 
-   onCancel: PropTypes.func.isRequired,
-   test_content: PropTypes.array.isRequired,
- };
+//  TestDialog.propTypes = {
+//    isOpen: PropTypes.bool.isRequired,
+//    onConfirm: PropTypes.func, 
+//    onCancel: PropTypes.func.isRequired,
+//    test_content: PropTypes.array.isRequired,
+//  };
