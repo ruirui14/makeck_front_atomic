e6a75755486b7a22fe35f2555290a126489dea9e
ruirui14 <2230103@ecc.ac.jp>
2026-01-18 19:16:02 +0900

    SelectRecipieSectionコンポーネント完成

--- src/component/organisms/SelectRecipieSection.jsx
+++ src/component/organisms/SelectRecipieSection.jsx
@@ -1,4 +1,37 @@
 // レシピ選択エリア
-function SelectRecipieSection() {
-    return
+import PropTypes from "prop-types";
+import RecipieList from "./RecipieList";
+import SectionLine from "../atoms/SectionLine";
+import CategoryButtonList from "../molecules/CategoryButtonList";
+
+function SelectRecipieSection({ menus, onCategorySelect, selectedId, onRecipeSelect }) {
+  return (
+    <>
+      {/*主食 主菜 副菜 汁物 遷移ボタン*/}
+      <div className="wrapButton">
+        <CategoryButtonList onSelect={onCategorySelect} />
+      </div>
+      {/*区切り線*/}
+      <SectionLine className="line" />
+      {/*説明文*/}
+      <div className="explanation">
+        <p>レシピを選択してください</p>
+      </div>
+      {/*レシピ選択コンテナ*/}
+      <RecipieList
+        menus={menus}
+        selectedId={selectedId}
+        onSelect={onRecipeSelect}
+      />
+    </>
+  );
 }
+
+SelectRecipieSection.propTypes = {
+  menus: PropTypes.array.isRequired,
+  selectedId: PropTypes.string,
+  onCategorySelect: PropTypes.func.isRequired,
+  onRecipeSelect: PropTypes.func.isRequired,
+};
+
+export default SelectRecipieSection;
